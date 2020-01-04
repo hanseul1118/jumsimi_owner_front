3a81ee107972f02eeb51f2d1f6ff76520e7cee86
hanseul.moon <haan1118@naver.com>
2020-01-04 22:16:33 +0900

    [화면] header 뒤로가기 컴포넌트 생성

---
+++ src/components/headerBack.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header-basic-container">
+        <div class="back-btn">뒤로</div>
+        <div class="header-txt">
+            점심이
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header-basic-container{
+        position: relative;
+
+        .back-btn{
+            position: absolute;
+            top: 0px;
+            left: 4px;
+        }
+        .header-txt{
+            text-align: center;
+            color: #F65130;
+        }
+    }
+</style>
