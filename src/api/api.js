885371bb7fa080fca4a8dcd5a373223c42ce3046
hanseul.moon <haan1118@naver.com>
2020-02-15 16:55:21 +0900

    [api.js] 메뉴리스트 조회 토큰 추가

--- src/api/api.js
+++ src/api/api.js
@@ -10,10 +10,13 @@ const API = {
     return axios.get(url, { params: request })
   },
   // 2. 메뉴 리스트
-  menuList (request) {
+  menuList (request, token) {
     let url = `${API_URL}/api/menu`
+    let headers = { 
+      Authorization: token
+    }
 
-    return axios.get(url, { params : request })
+    return axios.get(url, { params : request, headers: headers  })
   },
   // 3. 메뉴 상세 (가게 정보 포함)
   menuDetail (request, token) {
